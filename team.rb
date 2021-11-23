db939d03dc5c3e82619728d372257ca89295d593
Ubuntu <ubuntu@ip-172-31-4-99.ap-northeast-1.compute.internal>
2021-11-23 07:24:51 +0000

    kadai-ruby-oop-1

--- team.rb
+++ team.rb
@@ -22,18 +22,18 @@ end
 
 # インスタンスの生成
 # インスタンス名 = クラス名.new(引数)
-Giants = Team.new("Giants", 67, 45, 8)
-Tigers = Team.new("Tigers", 60, 53, 7)
-Dragons = Team.new("Dragons", 60, 55, 5)
-BayStars = Team.new("BayStars", 56, 58, 6)
-Carp = Team.new("Carp", 52, 56, 12)
-Swallows = Team.new("Swallows", 41, 69, 10)
+giants = Team.new("Giants", 67, 45, 8)
+tigers = Team.new("Tigers", 60, 53, 7)
+dragons = Team.new("Dragons", 60, 55, 5)
+baystars = Team.new("BayStars", 56, 58, 6)
+carp = Team.new("Carp", 52, 56, 12)
+swallows = Team.new("Swallows", 41, 69, 10)
 
 # インスタンスの実行
 # インスタンス名.メソッド名
-Giants.show_team_result
-Tigers.show_team_result
-Dragons.show_team_result
-BayStars.show_team_result
-Carp.show_team_result
-Swallows.show_team_result
+giants.show_team_result
+tigers.show_team_result
+dragons.show_team_result
+baystars.show_team_result
+carp.show_team_result
+swallows.show_team_result
